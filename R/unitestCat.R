c87864d6ad7e09cff3d5396802f0fdbebb321e3c
Aline Talhouk <atalhouk@bccrc.ca>
2016-03-03 12:47:02 -0800

    added drop level to by variable

--- R/unitestCat.R
+++ R/unitestCat.R
@@ -1,6 +1,6 @@
-unitestsCat <- function(fac.dat,fac.var,by, 
+unitestsCat <- function(fac.dat,fac.var,by,
                          per="col", digits=1){
-  #This function works for categoric data only. 
+  #This function works for categoric data only.
   #If not categorical error
   #determine how many categories in by
   if(is.factor(fac.dat[,by])){
@@ -8,32 +8,32 @@ unitestsCat <- function(fac.dat,fac.var,by,
   }else{
     stop('by variable must be factor')
   }
-  
+
   k <- length(fac.var)
-  
-  x=fac.dat[,fac.var[1]]
-  var=fac.var[1]
+
+
   # functions used
   sumStatsCat <- function(x,var, ind, digits) {
     x <- droplevels(x)
-    count <- table(x,ind) 
+    ind <- droplevels(ind)
+    count <- table(x,ind)
     na.r <- addmargins(table(x,ind, useNA = "always"))[length(levels(x))+1,-p-1]
     per.row <- prop.table(count,margin =1)
     per.col <- round(prop.table(addmargins(count,2), margin=2)*100,digits)
-    tots <- matrix(paste0(addmargins(count,2),"(", per.col,"%)"), byrow=F, 
+    tots <- matrix(paste0(addmargins(count,2),"(", per.col,"%)"), byrow=F,
                     nrow=dim(count)[1])%>%
        set_rownames(c(levels(x)))%>%
        rbind(Missing=na.r)%>%
        as.data.frame%>%
-       cbind(Variable=c(paste0("**",var,"**"), 
+       cbind(Variable=c(paste0("**",var,"**"),
                         rep("",length(levels(x)))),Levels=rownames(.),.)%>%
-      cbind(., AssociationTest=c(format(round(chisq.test(count)$p.value,3),nsmall=3), 
+      cbind(., AssociationTest=c(format(round(chisq.test(count)$p.value,3),nsmall=3),
                        rep("",length(levels(x)))))%>%
        set_rownames(NULL)%>%
        set_colnames(c("Variable","Levels",levels(ind),"Total","PValue"))
       return(list(count=count,tots=tots))
     }
-  
+
   # Obtain Summary Data
   ind <- fac.dat[, by]
   res=NULL
