c05c65efcd836ae8f1395eae428d86338c22c10f
ShanzhaoWang <swa162@sfu.ca>
2018-01-12 09:28:14 +0800

    An updated version of unitestCat.R with bugs fixed

--- R/unitestCat.R
+++ R/unitestCat.R
@@ -18,15 +18,13 @@ unitestsCat <- function(fac.dat, fac.var, fac.label, by,
   #If not categorical error
   #determine how many categories in by
 
-  if(is.factor(fac.dat[,by])){
-    p <- length(levels(fac.dat[,by]))
+  if(is.factor(fac.dat[, by])){
+    p <- length(levels(fac.dat[, by]))
   }else{
     stop('by variable must be factor')
   }
 
-  k <- length(fac.var)
-
-   # functions used
+   # Main functions used to obtain the marginal totals, which are the total counts of the cases over the categories of interest
   sumStatsCat <- function(x, var, var.lab, ind, digits) {
     x <- droplevels(x) # drop unused levels from a factor
     ind <- droplevels(ind)
@@ -36,23 +34,21 @@ unitestsCat <- function(fac.dat, fac.var, fac.label, by,
     per.row <- prop.table(count, margin =1)
     per.col <- round(prop.table(addmargins(count,2), margin = 2)*100, digits)
 
-    #
-    tots <- matrix(paste0(addmargins(count,2),"(", per.col,"%)"), byrow = F,
-                    nrow = dim(count)[1]) %>%
+    tots <- matrix(paste0(addmargins(count,2),"(", per.col,"%)"), byrow = F, nrow = dim(count)[1]) %>%
        set_rownames(c(levels(x)))
 
-    if(showMissing==TRUE){tots <- rbind(tots,Missing=na.r)}
+    # Missing cases will only be shown if there is one
+    if(sum(na.r) != 0){tots <- rbind(tots, Missing = na.r)}
 
     tots <- tots %>%
-        as.data.frame%>%
-       cbind(Variable=c(paste0("**",var.lab,"**"),rep("",nrow(.)-1)),Levels=rownames(.),.)%>%
-       cbind(., AssociationTest=c(format(round(chisq.test(count, simulate.p.value=simulate.p.value, B=B)$p.value,p.digits),nsmall=p.digits),
-                       rep("",nrow(.)-1)))%>%
-       set_rownames(NULL)%>%
-       set_colnames(c("Variable","Levels",levels(ind),"Total","PValue"))
-
+        as.data.frame %>%
+       cbind(Variable=c(paste0("**",var.lab,"**"),rep("",nrow(.)-1)),Levels=rownames(.),.) %>%
+       cbind(., AssociationTest=c(format(round(chisq.test(count, simulate.p.value=simulate.p.value, B=B)$p.value, p.digits), nsmall = p.digits),
+                       rep("", nrow(.)-1))) %>%
+       set_rownames(NULL) %>%
+       set_colnames(c("Variable", "Levels", levels(ind), "Total", "PValue"))
     return(list(count=count,tots=tots))
-    }
+  }
 
   # Obtain Summary Data
   ind <- fac.dat[, by]
