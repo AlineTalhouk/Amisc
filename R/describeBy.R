417024dad27c78b5b45513f02fd53a7a768e133b
ShanzhaoWang <swa162@sfu.ca>
2018-05-18 06:26:24 +0800

    warning fix

--- R/describeBy.R
+++ R/describeBy.R
@@ -9,7 +9,7 @@
 #' @author Aline Talhouk
 #' @export
 #' @import
-#' @importFrom (tidyr, extract), (reshape, expand)
+#' @importFrom tidyr extract
 #' @examples TODO
 
 describeBy <- function (data, var.names, var.labels = var.names, by1, dispersion="se", ShowTotal = TRUE,
