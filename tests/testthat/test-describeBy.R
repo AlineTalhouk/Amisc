795c88f9d393fe253fbc4dd61e50a2bfb538f6a9
Derek Chiu <dchiu@bccrc.ca>
2019-01-25 11:57:03 -0800

    add first unit test for describeBy

--- tests/testthat/test-describeBy.R
+++ tests/testthat/test-describeBy.R
@@ -1,1 +1,16 @@
 context("describeBy")
+
+mtcars$cyl <- as.factor(mtcars$cyl)
+mtcars$vs <- as.character(mtcars$vs)
+
+test_that("categorical variables can be summarized", {
+  r <- suppressWarnings(
+    Amisc::describeBy(data = mtcars, var.names = "vs", by1 = "cyl")
+  )
+  expect_equal(dim(r), c(3, 7))
+})
+
+test_that("numerical variables can be summarized", {
+  r <- Amisc::describeBy(data = mtcars, var.names = "hp", by1 = "cyl")
+  expect_equal(dim(r), c(3, 7))
+})
