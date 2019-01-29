990fed32eb73c985339227efcab54b3fec069f05
Derek Chiu <dchiu@bccrc.ca>
2019-01-28 16:40:39 -0800

    move helper funs definitions outside

--- R/unitestCat.R
+++ R/unitestCat.R
@@ -14,40 +14,6 @@ unitestsCat <- function(fac.dat, fac.var, fac.label, by, per = "col",
     stop("by variable must be factor")
   }
 
-  # Main functions used to obtain the marginal totals, which are the total counts of the cases over the categories of interest
-  sumStatsCat <- function(x, var, var.lab, ind, digits, per) {
-    x <- droplevels(x) # drop unused levels from a factor
-    ind <- droplevels(ind)
-    count <- table(x, ind, dnn = list(var, by))
-    na.r <- stats::addmargins(table(x, ind, useNA = "always"))[nlevels(x) + 1, -p - 1]
-    if (per == "col") {
-      per.val <- round(prop.table(stats::addmargins(count, margin = 2), margin = 2) * 100, digits)
-    } else if (per == "row") {
-      per.val <- round(stats::addmargins(prop.table(count, margin = 1), margin = 2) * 100, digits)
-    }
-    tots <- matrix(paste0(stats::addmargins(count, 2), " (", per.val, "%)"), byrow = FALSE, nrow = dim(count)[1]) %>%
-      magrittr::set_rownames(levels(x))
-
-    # Missing cases will only be shown if showMissing == TRUE and there are indeed missing ones
-    if (sum(na.r) != 0 && showMissing == TRUE) {
-      tots <- rbind(tots, Missing = na.r)
-    }
-    # re-arrange the matrix so that it will be able to rbind with continuous part
-    tot.rowname <- rownames(tots)
-    tots <- tots[, c(ncol(tots), 1:ncol(tots) - 1)]
-
-    tots <- as.data.frame(matrix(tots, ncol = nlevels(ind) + 1), row.names = tot.rowname) %>%
-      cbind(Variable = c(paste0("**", var.lab, "**"), rep("", nrow(.) - 1)), Levels = rownames(.), .) %>%
-      cbind(., AssociationTest = c(format(round(stats::chisq.test(count, simulate.p.value = simulate.p.value, B = B)$p.value, p.digits), nsmall = p.digits), rep("", nrow(.) - 1))) %>%
-      magrittr::set_rownames(NULL) %>%
-      magrittr::set_colnames(c("Variable", "Levels", "Total", levels(ind), "PValue")) %>%
-      dplyr::mutate_all(as.character)
-
-    tots <- tots[, c(1, 2, 4:(ncol(tots) - 1), 3, ncol(tots))] # re-arrange columns for the sake of output layout
-
-    return(list(count = count, tots = tots))
-  }
-
   # Obtain Summary Data
   ind <- fac.dat[, by]
   res <- NULL
@@ -55,4 +21,38 @@ unitestsCat <- function(fac.dat, fac.var, fac.label, by, per = "col",
     res <- rbind(res, sumStatsCat(factor(fac.dat[, fac.var[i]]), fac.var[i], fac.label[i], ind, digits, per)$tots)
   }
   return(list(formatted = res))
+}
+
+# Main functions used to obtain the marginal totals, which are the total counts of the cases over the categories of interest
+sumStatsCat <- function(x, var, var.lab, ind, digits, per) {
+  x <- droplevels(x) # drop unused levels from a factor
+  ind <- droplevels(ind)
+  count <- table(x, ind, dnn = list(var, by))
+  na.r <- stats::addmargins(table(x, ind, useNA = "always"))[nlevels(x) + 1, -p - 1]
+  if (per == "col") {
+    per.val <- round(prop.table(stats::addmargins(count, margin = 2), margin = 2) * 100, digits)
+  } else if (per == "row") {
+    per.val <- round(stats::addmargins(prop.table(count, margin = 1), margin = 2) * 100, digits)
+  }
+  tots <- matrix(paste0(stats::addmargins(count, 2), " (", per.val, "%)"), byrow = FALSE, nrow = dim(count)[1]) %>%
+    magrittr::set_rownames(levels(x))
+
+  # Missing cases will only be shown if showMissing == TRUE and there are indeed missing ones
+  if (sum(na.r) != 0 && showMissing == TRUE) {
+    tots <- rbind(tots, Missing = na.r)
+  }
+  # re-arrange the matrix so that it will be able to rbind with continuous part
+  tot.rowname <- rownames(tots)
+  tots <- tots[, c(ncol(tots), 1:ncol(tots) - 1)]
+
+  tots <- as.data.frame(matrix(tots, ncol = nlevels(ind) + 1), row.names = tot.rowname) %>%
+    cbind(Variable = c(paste0("**", var.lab, "**"), rep("", nrow(.) - 1)), Levels = rownames(.), .) %>%
+    cbind(., AssociationTest = c(format(round(stats::chisq.test(count, simulate.p.value = simulate.p.value, B = B)$p.value, p.digits), nsmall = p.digits), rep("", nrow(.) - 1))) %>%
+    magrittr::set_rownames(NULL) %>%
+    magrittr::set_colnames(c("Variable", "Levels", "Total", levels(ind), "PValue")) %>%
+    dplyr::mutate_all(as.character)
+
+  tots <- tots[, c(1, 2, 4:(ncol(tots) - 1), 3, ncol(tots))] # re-arrange columns for the sake of output layout
+
+  return(list(count = count, tots = tots))
 }
