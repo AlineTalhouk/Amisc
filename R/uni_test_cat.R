d07a0778d50e5916f0a19c71c8bf60dc9694b61a
Derek Chiu <dchiu@bccrc.ca>
2019-03-26 15:15:12 -0700

    refactor tots

--- R/uni_test_cat.R
+++ R/uni_test_cat.R
@@ -6,8 +6,8 @@
 #' @return a formatted summary of categorical variables
 #' @noRd
 uni_test_cat <- function(fac.dat, fac.var, fac.label, by, per = "col",
-                        digits = 0, p.digits = 3, showMissing,
-                        simulate.p.value = FALSE, B = 2000) {
+                         digits = 0, p.digits = 3, showMissing,
+                         simulate.p.value = FALSE, B = 2000) {
   # Verify `by` is a factor and store number of distinct levels
   if (is.factor(fac.dat[, by])) {
     level_num <- nlevels(fac.dat[, by])
@@ -27,34 +27,41 @@ uni_test_cat <- function(fac.dat, fac.var, fac.label, by, per = "col",
 }
 
 # Main functions used to obtain the marginal totals, which are the total counts of the cases over the categories of interest
-sum_stats_cat <- function(x, var, var.lab, ind, level_num, digits, per, p.digits, showMissing, simulate.p.value, B) {
+sum_stats_cat <- function(x, var, var.lab, ind, level_num, digits, per,
+                          p.digits, showMissing, simulate.p.value, B) {
   x <- droplevels(x) # drop unused levels from a factor
   ind <- droplevels(ind)
   count <- table(x, ind, dnn = list(var, by))
-  na.r <- stats::addmargins(table(x, ind, useNA = "always"))[nlevels(x) + 1, -level_num - 1]
   if (per == "col") {
-    per.val <- round(prop.table(stats::addmargins(count, margin = 2), margin = 2) * 100, digits)
+    per.val <- count %>%
+      stats::addmargins(margin = 2) %>%
+      prop.table(margin = 2)
   } else if (per == "row") {
-    per.val <- round(stats::addmargins(prop.table(count, margin = 1), margin = 2) * 100, digits)
+    per.val <- count %>%
+      prop.table(margin = 1) %>%
+      stats::addmargins(margin = 2)
   }
-  tots <- matrix(paste0(stats::addmargins(count, 2), " (", per.val, "%)"), byrow = FALSE, nrow = dim(count)[1]) %>%
-    magrittr::set_rownames(levels(x))
+  tots <- per.val %>%
+    magrittr::multiply_by(100) %>%
+    round(digits) %>%
+    paste0(stats::addmargins(count, 2), " (", ., "%)") %>%
+    matrix(nrow = nrow(count),
+           dimnames = list(levels(x), c(levels(ind), "Total")))
 
   # Missing cases will only be shown if showMissing == TRUE and there are indeed missing ones
-  if (sum(na.r) != 0 && showMissing == TRUE) {
+  na.r <- stats::addmargins(table(x, ind, useNA = "always"))[nlevels(x) + 1, -level_num - 1]
+  if (sum(na.r) != 0 && showMissing) {
     tots <- rbind(tots, Missing = na.r)
   }
   # re-arrange the matrix so that it will be able to rbind with continuous part
-  tot.rowname <- rownames(tots)
-  tots <- tots[, c(ncol(tots), 1:ncol(tots) - 1)]
-
-  tots <- as.data.frame(matrix(tots, ncol = nlevels(ind) + 1), row.names = tot.rowname) %>%
-    cbind(Variable = c(paste0("**", var.lab, "**"), rep("", nrow(.) - 1)), Levels = rownames(.), .) %>%
-    cbind(., AssociationTest = c(format(round(stats::chisq.test(count, simulate.p.value = simulate.p.value, B = B)$p.value, p.digits), nsmall = p.digits), rep("", nrow(.) - 1))) %>%
-    magrittr::set_rownames(NULL) %>%
-    magrittr::set_colnames(c("Variable", "Levels", "Total", levels(ind), "PValue")) %>%
-    dplyr::mutate_all(as.character)
-
-  tots <- tots[, c(1, 2, 4:(ncol(tots) - 1), 3, ncol(tots))] # re-arrange columns for the sake of output layout
+  test <- stats::chisq.test(count, simulate.p.value = simulate.p.value, B = B)$p.value
+  tots <- tots %>%
+    as.data.frame(stringsAsFactors = FALSE) %>%
+    dplyr::mutate(
+      Variable = c(paste0("**", var.lab, "**"), rep("", nrow(.) - 1)),
+      Levels = rownames(.),
+      PValue = c(format(round(test, p.digits), nsmall = p.digits), rep("", nrow(.) - 1))
+    ) %>%
+    dplyr::select(c("Variable", "Levels", levels(ind), "Total", "PValue"))
   tibble::lst(count, tots)
 }
